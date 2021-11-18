5280982d9a73232ae23c82a25da449aee1272add
ivansi <ivancogollos@hotmail.com>
2021-11-18 10:28:12 +0100

    Home.vue: Creamos botones de prueba

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Hola mundo</h1>
+
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secundary">Secundary</button>
+    <button class="btn btn-success">Success</button>
   </div>
 </template>
 
