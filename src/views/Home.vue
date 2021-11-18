3efbba196f823029e1b575e6a8df87102c8448c9
ivansi <ivancogollos@hotmail.com>
2021-11-18 10:46:21 +0100

    Home.vue: Corregimos error de escritura

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <h1>Hola mundo</h1>
 
     <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secundary">Secundary</button>
+    <button class="btn btn-secondary">Secondary</button>
     <button class="btn btn-success">Success</button>
   </div>
 </template>
